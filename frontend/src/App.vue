205492167e06583317efa43b31d72ac31e484a12
PETITE GOYAVE <goyave@VICTUSPierre.>
2025-03-06 16:31:11 +0100

    reglage de quelques problèmes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,15 +8,15 @@
 
 <script>
 import ParserEtudiant from './components/ParserEtudiant.vue';
-import AnneeAcademique from './components/AnneeAcademique.vue';
-import AppMessages from './components/AppMessages.vue';
+//import AnneeAcademique from './components/AnneeAcademique.vue';
+//import AppMessages from './components/AppMessages.vue';
 
 export default {
   name: 'App',
   components: {
-    ParserEtudiant,
-    AnneeAcademique,
-    AppMessages
+    ParserEtudiant//,
+    //AnneeAcademique,
+    //AppMessages
   }
 };
 </script>
